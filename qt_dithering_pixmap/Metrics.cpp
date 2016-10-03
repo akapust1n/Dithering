2b500b92f2828ed3826f9f843721500fddf412c0
djkah11 <djkah11@yandex.ru>
2016-10-04 02:16:54 +0300

    SSIM works for red

--- qt_dithering_pixmap/Metrics.cpp
+++ qt_dithering_pixmap/Metrics.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 double Metrics::getSSIM()
 {
-   std::cout<<"SSIM_____"<<ssim.getResult(image1, image2);
+   std::cout<<"SSIM_____"<<ssim.getResult(image1, image2)<<std::endl;
 }
 
 void Metrics::initFirstImage(QImage **image)
--- qt_dithering_pixmap/SSIM.cpp
+++ qt_dithering_pixmap/SSIM.cpp
@@ -13,8 +13,10 @@ double SSIM::getResult(QImage* _image1, QImage* _image2)
     //std::cout << "SR___________" << avg1 << std::endl;
     // auto var1= var(_image1, avg1);
     // std::cout << "VR___________" << var1 << std::endl;
-    std::cout<<"there";
+    std::cout << "there";
     oxy = cov(_image1, _image2, ux, uy);
+    std::cout << std::endl
+              << "Comparsion " << ox << " vs " << oxy << std::endl;
 
     return calculate(_image1->depth());
 }
@@ -31,13 +33,14 @@ double SSIM::avg(QImage* img)
         for (y = 0; y <= height; y++) {
             QColor pixel = img->pixel(x, y);
 
-            b = pixel.blue();
-            g = pixel.green(); // вот так мы как раз обращаемся к различным компонентам цветов
+            //   b = pixel.blue();
+            // g = pixel.green(); // вот так мы как раз обращаемся к различным компонентам цветов
             r = pixel.red(); // b - blue, r - red, g - green
-            col += b + g + r;
+            // col += b + g + r;
+            col += r;
         }
     }
-    return col / 3.00 / (width * height);
+    return col / (width * height);
 }
 
 double SSIM::var(QImage* img, double mu)
@@ -45,28 +48,29 @@ double SSIM::var(QImage* img, double mu)
 
     int width = img->width() - 1;
     int height = img->height() - 1;
-    int x, y, b = 0, g = 0, r = 0;
+    int x, y, b = 0, g = 0;
+    double r = 0;
     double col = 0;
 
     for (x = 0; x <= width; x++) {
         for (y = 0; y <= height; y++) {
 
             QColor pixel = img->pixel(x, y);
-            b = pixel.blue();
-            col += (abs(1.0 * b - mu)) * (abs(1.0 * b - mu));
-            g = pixel.green();
-            col += (abs(1.0 * g - mu)) * (abs(1.0 * g - mu));
-            r = pixel.red();
-            col += (abs(1.0 * r - mu)) * (abs(1.0 * r - mu));
+            // b = pixel.blue();
+            //  col += (abs(1.0 * b - mu)) * (abs(1.0 * b - mu));
+            // g = pixel.green();
+            //  col += (abs(1.0 * g - mu)) * (abs(1.0 * g - mu));
+            r = pixel.red() * 1.0;
+            col += abs(((r - mu)) * ((r - mu)));
         }
     }
-    return col/(width * height)/3.0;
+    return col / (width * height);
 }
 
 double SSIM::cov(QImage* img1, QImage* img2, double mu1, double mu2)
 { //covariance
 
-    int x, y, b1 = 0, g1 = 0, r1 = 0, b2 = 0, g2 = 0, r2 = 0;
+    int x, y, b1 = 0, g1 = 0, b2 = 0, g2 = 0, r1 = 0, r2 = 0;
     double col = 0;
     int width = img1->width() - 1;
     int height = img1->height() - 1;
@@ -76,25 +80,26 @@ double SSIM::cov(QImage* img1, QImage* img2, double mu1, double mu2)
             QColor pixel1 = img1->pixel(x, y);
             QColor pixel2 = img2->pixel(x, y);
 
-            b1 = pixel1.blue();
-            g1 = pixel1.green();
+            // b1 = pixel1.blue();
+            // g1 = pixel1.green();
             r1 = pixel1.red();
 
-            b2 = pixel2.blue();
-            g2 = pixel2.green();
+            // b2 = pixel2.blue();
+            // g2 = pixel2.green();
             r2 = pixel2.red();
-           // col+= (((b1-mu1)+(g1-mu1)+(r1-mu1))/3)*(((b2-mu2)+(g2-mu2)+(r2-mu2))/3);
-            col += (b1 - mu1) * (b2 - mu2) + (g1 - mu1) * (g2 - mu2) + (r1 - mu2) * (r2 - mu2);
+            col += abs((r1 * 1.0 - mu1) * (r2 *1.0 - mu2));
+            // col+= (((b1-mu1)+(g1-mu1)+(r1-mu1))/3)*(((b2-mu2)+(g2-mu2)+(r2-mu2))/3);
+            //col += (b1 - mu1) * (b2 - mu2) + (g1 - mu1) * (g2 - mu2) + (r1 - mu2) * (r2 - mu2);
         }
     }
 
-    return col/(width * height)/3;
+    return col / (width * height);
 }
 
 double SSIM::calculate(int64_t depth)
 {
     double result_part1 = (2 * ux * uy + getC1(depth)) * (2 * oxy + getC2(depth));
-    double result_part2 = (ux * ux + uy * uy + getC1(depth)) * (ox * ox + oy * oy + getC2(depth));
+    double result_part2 = (ux * ux + uy * uy + getC1(depth)) * (ox + oy + getC2(depth));
     return result_part1 / result_part2;
 }
 
